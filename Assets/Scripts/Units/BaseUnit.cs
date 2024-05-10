bf4b4633c594d2028df0d5a903bdf69ef904bd80
JulianJ99 <465268@student.fontys.nl>
2024-05-10 17:53:54 +0200

    Further changes

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -7,11 +7,46 @@ public class BaseUnit : MonoBehaviour {
     public Tile OccupiedTile;
     public Faction Faction;
 
-    //[SerializeField] private int health;
+    [SerializeField] private int health;
 
-    //[SerializeField] private int attack;
+    [SerializeField] private int attack;
 
-    //[SerializeField] private int defense;
+    [SerializeField] private int defense;
 
-    //[SerializeField] private int movementSpeed;
+    [SerializeField] private int movementRange;
+
+    [SerializeField] private int speed;
+
+    public void CharacterMovement(){
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (UnitManager.Instance.SelectedHero != null) {
+            Debug.Log("Selected");
+            UnitManager.Instance.SelectedHero.transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (UnitManager.Instance.SelectedHero != null) {
+            Debug.Log("Selected");
+            UnitManager.Instance.SelectedHero.transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (UnitManager.Instance.SelectedHero != null) {
+            Debug.Log("Selected");
+            UnitManager.Instance.SelectedHero.transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (UnitManager.Instance.SelectedHero != null) {
+            Debug.Log("Selected");
+            UnitManager.Instance.SelectedHero.transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+
+    }
 }
